fc9444b3da91b3925740cd05fed88da92d30b339
a6hishekyadav <abhiyad157@gmail.com>
2024-07-31 02:42:02 +0530

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = ({ user }) => {
   return (
     <div>
-      <div className="bg-[#f3dbdbb9] shadow-sm border-b-2 border-[#41625c]">
+      <div className="bg-[#f3eeedda] shadow-sm border-b-2 border-[#41625c]">
         <div className="mx-auto px-4 py-2 flex justify-between items-center">
           <Link to="/" className="flex items-center mr-5">
             <img
@@ -16,10 +16,10 @@ const Navbar = ({ user }) => {
           </Link>
 
           <div className="flex items-center space-x-4 w-[200px]">
-            <Link to="/" className="text-[#026C80] hover:text-[#064C72]">
+            <Link to="/" className="text-[#07181c] hover:text-[#0f648fbe]">
               Home
             </Link>
-            <Link to="/create" className="text-[#026C80] hover:text-[#064C72]">
+            <Link to="/create" className="text-[#07181c] hover:text-[#0f648fbe]">
               Create
             </Link>
             <Link
