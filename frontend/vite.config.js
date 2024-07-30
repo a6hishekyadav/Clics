82c4cfbd98e537c76eb25a82770cdefd03d49012
a6hishekyadav <abhiyad157@gmail.com>
2024-07-31 03:09:08 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL || "https://clics-backend.vercel.app/",
+          changeOrigin: true,
+          secure: false, // If using HTTPS
+        },
       },
     },
-  },
+  };
 });
